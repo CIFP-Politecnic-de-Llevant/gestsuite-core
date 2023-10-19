68b2d73ab2bcfdb0198ba07e5b9cb3bd9aad549c
Joan Galmés Riera <joan.galmes@gmail.com>
2023-10-19 21:35:33 +0200

    corregit bug que no permetia pujar fitxers a alumnes

--- src/main/java/cat/politecnicllevant/core/controller/GoogleStorageController.java
+++ src/main/java/cat/politecnicllevant/core/controller/GoogleStorageController.java
@@ -4,16 +4,20 @@ import cat.politecnicllevant.core.dto.google.FitxerBucketDto;
 import cat.politecnicllevant.core.service.GoogleStorageService;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.security.GeneralSecurityException;
+import java.util.Date;
+import java.util.Random;
 
 @RestController
 public class GoogleStorageController {
@@ -40,8 +44,31 @@ public class GoogleStorageController {
 
 
     @PostMapping("/googlestorage/uploadobject")
-    public ResponseEntity<FitxerBucketDto> uploadObject(@RequestParam("objectName") String objectName, @RequestParam("filePath") String filePath, @RequestParam("bucket") String bucket) throws IOException, GeneralSecurityException {
+    public ResponseEntity<FitxerBucketDto> uploadObject(@RequestParam("objectName") String objectName, @RequestParam("filePath") String filePath, @RequestParam("bucket") String bucket) throws IOException, GeneralSecurityException{
         FitxerBucketDto fitxerBucketDto = googleStorageService.uploadObject(objectName, filePath, bucket);
+        return new ResponseEntity<>(fitxerBucketDto, HttpStatus.OK);
+    }
+
+    @PostMapping("/googlestorage/uploadobjectfile")
+    public ResponseEntity<FitxerBucketDto> uploadObjectFile(@RequestParam("objectName") String objectName, @RequestParam("bucket") String bucket, @RequestPart(value = "file") final File uploadfile) throws IOException, GeneralSecurityException {
+        InputStream is = new FileInputStream(uploadfile);
+
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        byte[] readBuf = new byte[4096];
+        while (is.available() > 0) {
+            int bytesRead = is.read(readBuf);
+            os.write(readBuf, 0, bytesRead);
+        }
+
+        // Passam l'arxiu a dins una carpeta
+        String fileName = "/tmp/"+uploadfile.getName();
+
+        OutputStream outputStream = new FileOutputStream(fileName);
+        os.writeTo(outputStream);
+
+        File f = new File(fileName);
+
+        FitxerBucketDto fitxerBucketDto = googleStorageService.uploadObject(objectName, f.getAbsolutePath(), bucket);
         return new ResponseEntity<>(fitxerBucketDto, HttpStatus.OK);
     }
 
