d40708611623c294dc6b3b1c5b6454af87773782
Joan Galmés Riera <joan.galmes@gmail.com>
2023-10-24 09:22:25 +0200

    Modificació per a poder ser microserveis independents.

--- src/main/java/cat/politecnicllevant/core/controller/FitxerBucketController.java
+++ src/main/java/cat/politecnicllevant/core/controller/FitxerBucketController.java
@@ -5,11 +5,15 @@ import cat.politecnicllevant.core.service.FitxerBucketService;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -47,6 +51,12 @@ public class FitxerBucketController {
         return new ResponseEntity<>(path, HttpStatus.OK);
     }
 
+    @PostMapping(value = "/public/fitxerbucket/uploadlocal2",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> handleFileUpload2(@RequestPart(value = "file") final File uploadfile) throws IOException {
+        String path = saveUploadedFiles(uploadfile);
+        return new ResponseEntity<>(path, HttpStatus.OK);
+    }
+
     private String saveUploadedFiles(final MultipartFile file) throws IOException {
 
         Date ara = new Date();
@@ -61,6 +71,26 @@ public class FitxerBucketController {
         final byte[] bytes = file.getBytes();
         final Path path = Paths.get(absolutePath);
         Files.write(path, bytes);
+
+        System.out.println(absolutePath);
+        return absolutePath;
+    }
+
+    private String saveUploadedFiles(final File file) throws IOException {
+
+        Date ara = new Date();
+
+        Random random = new Random();
+        //Generate numbers between 0 and 1.000.000.000
+        int randomValue = random.nextInt(1000000001);
+
+        String absolutePath = StringUtils.stripAccents("/tmp/" + ara.getTime() + "_" +randomValue+"_"+ file.getName());
+        absolutePath = absolutePath.replaceAll("\\s+","");
+
+        File fileLocal = new File(absolutePath);
+        OutputStream out = new FileOutputStream(file);
+        // Write your data
+        out.close();
 
         System.out.println(absolutePath);
         return absolutePath;
