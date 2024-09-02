d5cd035a994967b259f13180092622ca73f356c7
Joan Galmés <joan.galmes@gmail.com>
2024-09-02 11:04:18 +0200

    added curs academic

--- src/main/java/cat/politecnicllevant/core/repository/gestib/CursAcademicRepository.java
+++ src/main/java/cat/politecnicllevant/core/repository/gestib/CursAcademicRepository.java
@@ -1,6 +1,5 @@
 package cat.politecnicllevant.core.repository.gestib;
 
-import cat.politecnicllevant.core.model.gestib.Curs;
 import cat.politecnicllevant.core.model.gestib.CursAcademic;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
