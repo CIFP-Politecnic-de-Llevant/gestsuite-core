f49b84c19c7b592f680e1478d160d3a25c9eee6e
Joan Galmés <joan.galmes@gmail.com>
2024-09-02 13:28:54 +0200

    added convocatoria

--- src/main/java/cat/politecnicllevant/core/repository/gestib/CursAcademicRepository.java
+++ src/main/java/cat/politecnicllevant/core/repository/gestib/CursAcademicRepository.java
@@ -4,8 +4,6 @@ import cat.politecnicllevant.core.model.gestib.CursAcademic;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface CursAcademicRepository extends JpaRepository<CursAcademic, Long> {
 }
